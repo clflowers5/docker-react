01c78bb3160b5f2346c722d4e79a6668c6f1fcf0
Chris Flowers <clflowers5@gmail.com>
2019-06-07 10:54:28 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,16 +7,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>shoop da woop silly man</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React and all the good stuffs
         </a>
       </header>
     </div>
